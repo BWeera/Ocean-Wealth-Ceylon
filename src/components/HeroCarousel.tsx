2376e287180d6f7bd8f520558883c657ca990896
Bimsara Weerakoon <bimsaraweerakoon@gmail.com>
2026-04-28 10:28:28 +0530

    fix: update description text in HeroCarousel for clarity and improved messaging

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -69,7 +69,7 @@ export default function HeroCarousel() {
             Fresh From the Ocean to You
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-300 drop-shadow">
-            Ocean Wealth Ceylon bringing you the finest and freshest aquatic products meticulously sourced from the deep blue. Experience unparalleled quality for your culinary endeavors.
+            Ocean Wealth Ceylon bringing you the finest and freshest aquatic products meticulously sourced from the cristal clear blue ocean of Sri Lanka. Experience unmatched quality for your culinary endeavors.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
